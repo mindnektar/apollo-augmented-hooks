ff79b5e6d07db644e37762e5f4a837bee9df83ac
Martin Denk <martin.denk@carloandfriends.de>
2023-03-10 17:54:55 +0100

    fix(queries): fix refetching queries by adding context and skip options to cache query

--- src/hooks/useCacheQuery.js
+++ src/hooks/useCacheQuery.js
@@ -1,15 +1,22 @@
 import { useQuery } from '@apollo/client';
 import apolloClient from '../apolloClient';
 import mapData from '../helpers/mapData';
+import { useGlobalContext } from '../globalContextHook';
 
 export default (queryAst, variables, options) => {
     const client = apolloClient();
+    const globalContext = useGlobalContext();
 
     // Grab all the requested data from the cache. If some or all of the data is missing, the reduced query will get it.
     const cacheResult = useQuery(queryAst, {
         client,
         variables,
         fetchPolicy: 'cache-only',
+        skip: options.skip,
+        context: {
+            ...globalContext,
+            ...options.context,
+        },
     });
 
     return mapData(cacheResult.data, options.dataMap);
